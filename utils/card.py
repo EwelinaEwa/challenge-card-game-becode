0fff41f8122be5a5c794a4461f6663e76b8df062
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-09-07 10:45:44 +0200

    Add Card class

--- utils/card.py
+++ utils/card.py
@@ -10,3 +10,16 @@ class Symbol:
     def __str__(self):
         return self.icon
 
+
+class Card (Symbol):
+
+    value = ['A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K']
+
+    def __init__(self, value, color, icon):
+        super(Card, self).__init__(color, icon)
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
